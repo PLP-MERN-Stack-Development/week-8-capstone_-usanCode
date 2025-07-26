7c6723b6c21fb8dad6f0eff745863d5cb156b873
usanCode <sabinerugugula@gmail.com>
2025-07-26 20:18:46 +0200

    backend: updated socketjs file by adding the Render backend url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,11 @@
 
 
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000"); // or your deployed backend URL
+
+const socket = io(
+  import.meta.env.PROD
+    ? "https://week-8-capstone-usancode.onrender.com"
+    : "http://localhost:5000"
+);
 
 export default socket;
